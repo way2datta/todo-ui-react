c6edfbe4b35a3d3ec7307074cf2fdf24c9670556
Dattatray Kale <way2datta@gmail.com>
2021-08-21 23:11:21 +0530

    Fix more lint errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import "../styles/App.css";
+import '../styles/App.css';
 import React from 'react';
 import TodoInput from '../components/TodoInput';
 import generateRandomText from '../libs/randomString';
